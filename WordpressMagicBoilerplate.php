8b2a150f428e25cd9c0e29de734893bcdaea3478
petrozavodsky <petrozavodsky@gmail.com>
2018-04-10 20:55:48 +0300

    share

---
+++ WordpressMagicBoilerplate.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Plugin Name: Ml Recommendation Block
+Plugin URI: http://alkoweb.ru
+Author: Petrozavodsky Vladimir
+Author URI: http://alkoweb.ru
+Requires PHP: 7.0
+*/
+	
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require_once( plugin_dir_path( __FILE__ )."includes/Autoloader.php" );
+
+use MlRecommendationBlock\Autoloader;
+
+new Autoloader( __FILE__, 'MlRecommendationBlock' );
+
+
+use MlRecommendationBlock\Base\Wrap;
+use MlRecommendationBlock\Utils\ActivateWidgets;
+
+class MlRecommendationBlock extends Wrap {
+	public $version = '1.0.0-rc.3';
+	public static $textdomine;
+
+	function __construct() {
+		self::$textdomine = $this->setTextdomain();
+
+		new ActivateWidgets(
+			__FILE__,
+			'Widgets',
+			'MlRecommendationBlock'
+		);
+
+
+	}
+
+}
+
+function MlRecommendationBlock__init() {
+	new MlRecommendationBlock();
+}
+
+add_action( 'plugins_loaded', 'MlRecommendationBlock__init', 30 );
