e4f4c85a49904df8e2085ef10a5cc378fafa8be4
petrozvodsky <petrozavodsky@gmail.com>
2018-04-11 10:14:05 +0300

    мфдшвфеу ьуерщв

--- src/MlRecommendationBlock/Widgets/MagicWidget.php
+++ src/MlRecommendationBlock/Widgets/MagicWidget.php
@@ -6,45 +6,75 @@ use MlRecommendationBlock;
 use MlRecommendationBlock\Classes\QueryParams;
 use WP_Widget;
 
-class MagicWidget extends WP_Widget
-{
-    private $suffix = " - ML widget";
+class MagicWidget extends WP_Widget {
+	private $suffix = " - ML widget";
 
-    function __construct()
-    {
-        $className = get_called_class();
-        $className = str_replace("\\", '-', $className);
+	function __construct() {
+		$className = get_called_class();
+		$className = str_replace( "\\", '-', $className );
 
-        parent::__construct(
-            $className,
-            __("Recommendation ", 'MlRecommendationBlock') . $this->suffix,
-            [
-                'description' => __("Recommendation block widget", 'MlRecommendationBlock') . $this->suffix
-            ]
-        );
-    }
+		parent::__construct(
+			$className,
+			__( "Recommendation ", 'MlRecommendationBlock' ) . $this->suffix,
+			[
+				'description' => __( "Recommendation block widget", 'MlRecommendationBlock' ) . $this->suffix
+			]
+		);
+	}
 
-    public function widget($args, $instance)
-    {
-        new QueryParams();
-        echo $args['before_widget'];
-        ?>
+	public function widget( $args, $instance ) {
+		$title          = apply_filters( 'widget_title', $instance['title'] );
 
-        <?php
-        echo $args['after_widget'];
-    }
+		echo $args['before_widget'];
+		?>
+        <h1>1224545454544</h1>
+		<?php
+		echo $args['after_widget'];
+	}
 
 
-    public function form($instance)
-    {
-        echo '<p class="no-options-widget">' . __('There are no options for this widget.', 'MlRecommendationBlock') . '</p>';
+	public function form( $instance ) {
 
-        return 'noform';
-    }
+		?>
+        <p>
+            <label for="<?php echo $this->get_field_id( 'title' ); ?>">Заголовок</label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>"
+                   name="<?php echo $this->get_field_name( 'title' ); ?>"
+                   type="text"
+                   value="<?php echo esc_attr( $instance['title'] ); ?>"/>
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id( 'posts_per_page' ); ?>">
+				<?php _e( 'Number of posts:', 'MlRecommendationBlock' ); ?>
+            </label>
+            <input id="<?php echo $this->get_field_id( 'posts_per_page' ); ?>"
+                   name="<?php echo $this->get_field_name( 'posts_per_page' ); ?>"
+                   type="number" value="<?php echo $instance['posts_per_page']; ?>"
+                   size="3"/>
+        </p>
+		<?php
+	}
 
-    public function update($new_instance, $old_instance)
-    {
-        return $new_instance;
-    }
+	public function update( $new_instance, $old_instance ) {
+
+	    $instance = $this->validate($new_instance);
+
+		return $instance;
+	}
+
+	private function validate( $array ) {
+
+
+		array_walk( $array, function ( $v, $k ) {
+			if ( 'title' == $k ) {
+				$array[ $k ] = ( ! empty( $v ) ) ? strip_tags( $v ) : '';
+			} else if ( 'posts_per_page' == $k ) {
+				$array[ $k ] = ( is_numeric( $v ) ) ? intval( $v ) : 8;
+			}
+
+		} );
+
+		return $array;
+	}
 
 }
