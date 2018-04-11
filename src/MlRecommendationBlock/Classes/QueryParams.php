f46518a8136aaf922987a8b2dab617f3426b892a
petrozvodsky <petrozavodsky@gmail.com>
2018-04-11 17:11:01 +0300

    change generate props

--- src/MlRecommendationBlock/Classes/QueryParams.php
+++ src/MlRecommendationBlock/Classes/QueryParams.php
@@ -2,47 +2,43 @@
 
 namespace MlRecommendationBlock\Classes;
 
-class QueryParams
-{
-    public $excluded = [];
+class QueryParams {
+	public $excluded = [];
 
-    public $tags = [];
+	public $tags = [];
 
-    public $date_qyery =[];
+	public $date_qyery = [];
 
-    public function __construct($days)
-    {
-        $this->excluded();
-        $this->date_offset($days);
-        if (is_singular()) {
-            $this->tags($GLOBALS['wp_query']->queried_object_id);
-        }
+	public function __construct( $days ) {
+		$this->excluded();
+		$this->date_offset( $days );
+		if ( is_singular() ) {
+			$this->tags( $GLOBALS['wp_query']->queried_object_id );
+		} else {
+			$this->tags( $GLOBALS['wp_query']->posts[0]->ID );
+		}
+	}
 
-    }
+	private function date_offset( $days = 3 ) {
+		$this->date_qyery = [
+			[
+				'after' => "{$days} day ago"
+			]
+		];
 
-    private function date_offset($days=3)
-    {
-        $this->date_qyery = [
-        [
-            'after' => "{$days} day ago"
-        ]
-    ];
+	}
 
-    }
+	private function excluded() {
+		$this->excluded = array_map(
+			function ( $elem ) {
+				return $elem->ID;
+			},
+			$GLOBALS['wp_query']->posts
+		);
+	}
 
-    private function excluded()
-    {
-        $this->excluded = array_map(
-            function ($elem) {
-                return $elem->ID;
-            },
-            $GLOBALS['wp_query']->posts
-        );
-    }
-
-    private function tags($post_id)
-    {
-        $this->tags = wp_get_post_tags($post_id, ['fields' => 'ids']);
-    }
+	private function tags( $post_id ) {
+		$this->tags = wp_get_post_tags( $post_id, [ 'fields' => 'ids' ] );
+	}
 
 }
